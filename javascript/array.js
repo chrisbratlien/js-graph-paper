5526501b271e8db1007c143323ecf92f8530db06
Chris Bratlien <chrisbratlien@gmail.com>
2023-08-19 07:18:49 -0500

    self-host some JS

--- javascript/array.js
+++ javascript/array.js
@@ -1,54 +1,117 @@
-///////////Array prototype versions //////////
-Array.prototype.collect = function(fn) { 
-  var result = [];
-  for (var i = 0; i < this.length; i += 1) {
-    result.push(fn(this[i]));
-  }
-  return result;
-};
+﻿Array.prototype.collect = Array.prototype.map;
 
-//map is same as collect
-Array.prototype.map = Array.prototype.collect;
 
 //select returns an array containing only the elements which pass the test
-Array.prototype.select = function(test) {
-  var result = [];
-  for (var i = 0; i < this.length; i += 1) {
-    if (test(this[i])) {
-      result.push(this[i]);
+Array.prototype.select = function (test) {
+    var result = [];
+    for (var i = 0; i < this.length; i += 1) {
+        if (test(this[i])) {
+            result.push(this[i]);
+        }
     }
-  }
-  
-  return result;
+
+    return result;
 };
+
+Array.prototype.reject = function(test) {
+  return this.select(function(element) { return !test(element); });
+
+}
+
+
 
 //detect returns the first element which passes the test, or false. aka "find"
-Array.prototype.detect = function(test) {
-  for (var i = 0; i < this.length; i += 1) {
-    if (test(this[i])) {
-      return this[i];
+Array.prototype.detect = function (test) {
+    for (var i = 0; i < this.length; i += 1) {
+        if (test(this[i])) {
+            return this[i];
+        }
     }
-  }
-  
-  return false;
+
+    return false;
 };
 
 
-Array.prototype.contains = function(x) { //does the array contain x?
-  var result = this.detect(function(n) { return n == x; });    
-  return (result !== false);
+Array.prototype.contains = function (x) { //does the array contain x?
+    var result = this.detect(function (n) { return n == x; });
+    return (result !== false);
 };
 
 
 //each runs a block of code on each element
-Array.prototype.each = function(fn) {
-  for (var i = 0; i < this.length; i += 1) {
-    fn(this[i]);
-  }
+Array.prototype.each = function (fn) {
+    for (var i = 0; i < this.length; i += 1) {
+        fn(this[i]);
+    }
 };
 
-Array.prototype.atRandom = function() {
-if (this.length === 0) { return false; }
-  var i = Math.floor(Math.random()*this.length);
-  return this[i];
+Array.prototype.inject = function (acc, fn) {
+    var result = acc;
+    this.each(function (e) {
+        result = fn(result, e);
+    });
+    return result;
 };
+
+
+
+Array.prototype.atRandom = function () {
+    if (this.length === 0) { return false; }
+    var i = Math.floor(Math.random() * this.length);
+    return this[i];
+};
+
+Array.prototype.integrate = function (fn) {
+    return this.inject(0, function (acc, i) { return acc + fn(i); });
+};
+
+Array.prototype.sum = function () {
+    return this.integrate(function (x) { return x; });
+};
+
+ Array.prototype.average = function() {
+  return this.integrate(function(x) {return x; }) / this.length;
+};
+
+
+
+/* IE doesn't support Array.indexOf, so here, hold my hand while we cross the street */
+if (!Array.indexOf) {
+    Array.prototype.indexOf = function (obj) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == obj) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+Array.prototype.eachPCN = function(callback) { //gives your callback a view of previous, current, and next
+
+    var length = this.length;
+    var c,p,n = false;
+    switch(length) {
+      case 0:
+        console.log('err-OR! does not compute. ');
+        break;
+      case 1:
+        c = 0; n = 0; p = 0;
+        break;
+      case 2: 
+        c = 0; n = 1; p = 1;
+        break;
+      default:
+        c = 0; n = 1; p = length - 1;
+        break;
+    }    
+    
+    for(var i = 0; i < length; i += 1) {
+      callback({ prev: this[p], current: this[c], next: this[n], p: p, c: c, n: n, length: length });
+      c += 1; c %= length;
+      n += 1; n %= length;
+      p += 1; p %= length;
+    }
+};
+
+
---
+++ javascript/color.js
@@ -0,0 +1,190 @@
+if (typeof BSD == "undefined") {
+  BSD = {};
+}
+
+
+BSD.colorFromHex = function(hex) {
+  var h = hex.replace('#','');
+  return BSD.Color({
+    r: parseInt(h.substr(0,2),16),
+    g: parseInt(h.substr(2,2),16),
+    b: parseInt(h.substr(4,2),16)
+  });
+};
+
+BSD.randHex = function() {
+  return ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'].atRandom();
+};
+BSD.randomColor = function() {
+  var result = '#';
+  for (var i = 0; i < 6; i += 1) {
+    result += BSD.randHex();
+  }
+  return BSD.colorFromHex(result);
+};
+
+BSD.randomDarkColor = function() {
+  var color = BSD.randomColor();
+  while (! color.isSomewhatDark()) {
+    color = BSD.randomColor();  
+  }
+  /////console.log('#' + color.toHex());
+  return color;
+};
+
+
+BSD.randomPalette = function(size,threshold) {
+  var results = [];
+  results.push(BSD.randomDarkColor());
+  while(results.length < size) {
+    var test = BSD.randomDarkColor();
+    var distances = results.collect(function(rc) { return rc.distanceTo(test); });
+    distances.sort(function(a,b) { return a > b; });
+    var closest = distances[0];
+    //console.log(distances,'distances');
+    //console.log(closest,'closest');
+
+    if (closest < threshold) {
+      ////console.log('skipping',test.toHex(),closest);  
+    }
+    else {
+      results.push(test);    
+    }
+  } 
+  return results;
+}
+
+
+
+BSD.randomPalette2 = function(size,threshold) {
+  var results = [];
+  
+  var first = BSD.randomColor();  
+  results.push(first);
+
+  var last = first;    
+  while(results.length < size) {
+    var test = BSD.randomColor();
+    var distance = Math.abs(test.distanceTo(last));
+    while (distance < (threshold - threshold*0.1) || distance > (threshold + threshold*0.1)) {
+      var test = BSD.randomColor();  
+      var distance = Math.abs(test.distanceTo(last));
+    }    
+    ////console.log('distance',distance);
+    results.push(test);    
+    last = test;
+  }      
+  return results;
+};
+
+
+BSD.Color = function(spec){
+  var self = {};
+  self.r = spec.r;
+  self.g = spec.g;
+  self.b = spec.b;
+
+  self.brightness = function() {
+    return self.r + self.g + self.b;
+  };
+  
+  self.isBright = function() {
+    return self.brightness() > 384;
+  };
+  self.isDark = function() {
+    return ! self.isBright();
+  };  
+  self.isSomewhatDark = function() {
+    var b = self.brightness();
+    
+    return (b > 128 && b < 480);
+    ///return self.brightness() < 256;
+    ///return self.brightness() < 300;
+  };
+  self.isReallyDark = function() {
+    return self.brightness() < 192;
+  };
+  self.distanceTo = function(other) {
+    var dr = self.r - other.r;
+    var dg = self.g - other.g;
+    var db = self.b - other.b;
+    return Math.sqrt(dr*dr + dg*dg + db*db);
+  };
+  
+  
+  self.complement = function() {
+    return BSD.Color({
+      r: 255 - self.r,
+      g: 255 - self.g,
+      b: 255 - self.b
+    });
+  }
+  
+  
+  
+  
+  self.equals = function(other) {
+    return (
+      self.r == other.r &&
+      self.g == other.g &&
+      self.b == other.b
+    );  
+  };
+
+  self.minus = function(other) {
+    return BSD.Color({  
+      r: self.r - other.r,
+      g: self.g - other.g,
+      b: self.b - other.b
+    });  
+  };
+  
+  self.plus = function(other) {
+    return BSD.Color({  
+      r: self.r + other.r,
+      g: self.g + other.g,
+      b: self.b + other.b
+    });  
+  };
+
+  self.dividedBy = function(x) {
+    return BSD.Color({  
+      r: self.r / x,
+      g: self.g / x,
+      b: self.b / x
+    });  
+  };
+  self.delta = function(other) {
+    return other.minus(self);
+  };
+  self.upTo = function(other,steps) {
+    
+    var result = [];
+    var delta = self.delta(other).dividedBy(steps);  
+    ////console.log(delta,'delta');
+    var next = self;
+
+    result.push(next);
+    for(var i = 0; i < steps; i += 1) {
+      next = next.plus(delta);
+      result.push(next);
+    }
+    return result;
+  };
+  self.toHex = function() {
+    var result = ('00' + Math.floor(self.r).toString(16)).substr(-2) + ('00' + Math.floor(self.g).toString(16)).substr(-2) + ('00' + Math.floor(self.b).toString(16)).substr(-2);
+    ///console.log(result);
+    return result;
+  };
+  
+  
+  self.renderOn = function(html) {
+  
+    var str = self.toHex();
+    ////console.log(str,'str');
+  
+    jQuery(html).css('background-color','#' + str);
+  }
+  
+  return self;
+}
